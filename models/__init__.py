e4e2daa90ed4f4fd8557eb343664432742e61048
FlappyIce <flappyice73@outlook.com>
2021-03-09 02:37:00 -0500

    added model

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 import mongoengine as __mongo
 from configs import auth
+import logging
 
 from .__video import Video  # noqa
 from .__up import Up, VerifiedUp  # noqa
 
 __mongo.connect(db='ttd', authentication_source='admin', host=auth.mongo_url)
+logging.info('db connected, models init success')
