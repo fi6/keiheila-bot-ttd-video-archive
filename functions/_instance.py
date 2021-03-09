79e74cdfcd01da04abbc5df1b1fd3ed50e58e60b
FlappyIce <flappyice73@outlook.com>
2021-03-09 02:56:41 -0500

    added logging for 1

--- functions/_instance.py
+++ functions/_instance.py
@@ -1,6 +1,7 @@
 import os
 from khl import Bot, Cert
 from configs import auth
+import logging
 cert = Cert(client_id=auth.khl_client,
             client_secret=auth.khl_secret,
             token=auth.khl_token)
@@ -8,3 +9,11 @@ cert = Cert(client_id=auth.khl_client,
 bot = Bot(cmd_prefix=['.', '。'], cert=cert)
 
 
+async def async_logging(x):
+    try:
+        logging.info(x) if x.content == '1' else None
+    except Exception as e:
+        e
+
+
+bot.on_text_msg(async_logging)
--- test/bot.py
+++ test/bot.py
@@ -3,10 +3,4 @@ sys.path.append('.')
 
 from functions import bot
 
-
-async def async_print(x):
-    print(x)
-
-
-bot.on_raw_event(async_print)
 bot.run()
