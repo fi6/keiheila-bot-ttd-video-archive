afc2ec2f86f77ed56c264b77dac7be353e136781
FlappyIce <flappyice73@outlook.com>
2021-03-09 02:36:07 -0500

    minor change

--- core/__polling/__init__.py
+++ core/__polling/__init__.py
@@ -2,6 +2,7 @@ from pyee import AsyncIOEventEmitter
 from ._check_live import check_living
 from ._check_video import check_video
 import signal
+import logging
 
 
 def signal_handler(signal, frame):
@@ -18,13 +19,18 @@ class __Polling(AsyncIOEventEmitter):
     counter: int = 0
 
     async def start(self):
+        logging.info('polling start')
         while True:
             living = check_living()
             if living:
+                logging.info(
+                    'live start event found {user}'.format(user=living.name))
                 self.emit('live_start', living)
             if self.counter == 15:
                 self.counter = 0
                 for video in check_video():
+                    logging.info('new video: {user} {title}'.format(
+                        user=video.author, title=video.title))
                     self.emit('video_update', video)
             self.counter += 1
             if interrupted:
---
+++ run.sh
@@ -0,0 +1,22 @@
+NOW=$(date +"%H-%M-%S")
+DATE=$(date +"%m-%d-%y")
+mkdir -p logs/$DATE
+export PROJECT_DIR=/home/fi/tools/tou-ladder
+PID=$(cat run.pid)
+PID_EXIST=$(ps aux | awk '{print $2}'| grep -w $PID)
+if ps -p $PID > /dev/null
+then
+   echo "$PID is running"
+   kill $PID
+   echo "$PID killed"
+   # Do something knowing the pid exists, i.e. the process with $PID is running
+fi
+source "$PROJECT_DIR"/.venv/bin/activate
+nohup python "$PROJECT_DIR"/app.py &> "$PROJECT_DIR"/logs/$DATE/$NOW.log & echo $! > run.pid
+echo "run at pid $(cat run.pid)"
+unset -v latest
+sleep 1
+for file in "$PROJECT_DIR"/logs/*/*; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+tail -vf -n 30 $file
