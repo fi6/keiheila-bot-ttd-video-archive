4807c70eecd8760f5db42342082c069cbfbbc375
FlappyIce <flappyice73@outlook.com>
2021-03-12 08:59:02 -0500

    update

--- core/uplist.py
+++ core/uplist.py
@@ -4,6 +4,7 @@ uplist = {
     'naisi': 9409346,
     'shisi': 1408305,
     'baye': 7926167,
-    'moria': 2728271
+    'moria': 2728271,
+    'srf': 793755
 }
 roomlist = {'dio': 9900640, 'naisi': 22528524}
