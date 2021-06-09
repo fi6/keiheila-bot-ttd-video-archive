2f206c03a4898469ef6fca8fdae9a0ddd822862b
FlappyIce <flappyice73@outlook.com>
2021-06-09 02:19:17 -0400

    new add verified up method

--- utils/add_verified_up.py
+++ utils/add_verified_up.py
@@ -1,27 +1,41 @@
 from bilibili_api import user
 import sys
+
+from mongoengine.errors import DoesNotExist
 sys.path.append('.')
 
 from models import VerifiedUp
+from functions import bot
 
 # ups = VerifiedUp.objects(tag='video', priority=1)
 # print(ups)
 
-# mandom: 853821709  con：1404213443
+# mandom: 853821709
 
-bilibili_id = 390851181
-kaiheila_id = 1404213443
-live = True
+bilibili_id = 47229249
+kaiheila_id = 1861915305
+live = False
 priority = -1  # -1, 0, 1 low->high
 
 u = user.get_user_info(int(bilibili_id))
-up = VerifiedUp(uid=u['mid'],
-                roomid=u['live_room']['roomid'],
-                avatar=u['face'],
-                nickname=u['name'],
-                sign=u['sign'],
-                priority=priority,
-                tag=['video'] if not live else ['video', 'live'])
-up.kid = str(kaiheila_id)
+try:
+    up = VerifiedUp.objects.get(uid=bilibili_id)
+    up.update(uid=u['mid'],
+              roomid=u['live_room']['roomid'],
+              avatar=u['face'],
+              nickname=u['name'],
+              sign=u['sign'],
+              priority=priority,
+              tag=['video'] if not live else ['video', 'live'],
+              kid=str(kaiheila_id))
+except DoesNotExist:
+    up = VerifiedUp(uid=u['mid'],
+                    roomid=u['live_room']['roomid'],
+                    avatar=u['face'],
+                    nickname=u['name'],
+                    sign=u['sign'],
+                    priority=priority,
+                    tag=['video'] if not live else ['video', 'live'],
+                    kid=str(kaiheila_id))
 up.save()
 print(up.to_mongo())
